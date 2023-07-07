887324b2d916496854a09c202cd538f2fb5c1fde
Connor Van Etten <87097190+Crvanetten15@users.noreply.github.com>
2023-07-07 15:16:48 -0400

    File structure and messing with buttons

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function App() {
+const AppButton = ({ onPress, icon, title, backgroundColor }) => (
+  <View style={styles.appButtonContainer}>
+    <Icon.Button
+      name={icon}
+      backgroundColor={backgroundColor}
+      onPress={onPress}
+      style={styles.appButton}
+    >
+      <Text style={styles.appButtonText}>{title}</Text>
+    </Icon.Button>
+  </View>
+);
+
+// TouchableOpacity.defaultProps = { activeOpacity: 0.3 };
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <AppButton icon="sign-in" title="Login with password" backgroundColor="#777"/>
+      <AppButton icon="facebook" title="Login with Facebook" backgroundColor="#3b5998"/>
+      <AppButton icon="github" title="Login with GitHub" backgroundColor="#6e5494"/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 80,
+    backgroundColor: "#555",
+  },
+  appButton: {
+    padding: 12,
+  },
+  appButtonText: {
+    fontSize: 17,
+  },
+  appButtonContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 12,
   },
 });
+
+export default App;
